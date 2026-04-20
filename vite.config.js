feccaa66334e413401fe6f487028f0e990d5fdd5
Deb Guin <sukumarguin10@gmail.com>
2026-04-20 14:15:25 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,14 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist",
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
   server: {
-    // Proxy /api/* to a local dev server during development
-    // Run `vercel dev` instead of `npm run dev` to test the API routes locally
     proxy: {},
   },
 });
